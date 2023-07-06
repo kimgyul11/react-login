580c3589baa866a46c45568a2485702ea2bdcd53
kimgyul11 <coloo88@naver.com>
2023-07-06 14:37:19 +0900

    라우터 설정 완료

--- src/App.js
+++ src/App.js
@@ -1,5 +1,22 @@
-function App() {
-  return <div></div>;
-}
+import React, { useState } from "react";
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
+import Home from "./routes/Home";
+import Auth from "./routes/Auth";
 
-export default App;
+const useRouter = () => {
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+  const router = createBrowserRouter([
+    { path: "/", element: isLoggedIn ? <Home /> : <Auth /> },
+    { path: "login", element: <Auth /> },
+  ]);
+  return router;
+};
+
+export default function App() {
+  const router = useRouter();
+  return (
+    <RouterProvider router={router}>
+      <div></div>
+    </RouterProvider>
+  );
+}
--- src/index.js
+++ src/index.js
@@ -1,11 +1,16 @@
-import React from "react";
+import React, { useState } from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 
 import { firebase } from "./api/filebase";
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
+import Home from "./routes/Home";
+import Auth from "./routes/Auth";
 
 console.log(firebase);
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
+
 root.render(
   <React.StrictMode>
     <App />
