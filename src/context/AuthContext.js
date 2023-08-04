843473cabe99e7b9d9025231e70872523d31f75a
kimgyul11 <coloo88@naver.com>
2023-08-04 20:49:16 +0900

    프로필 수정 기능-완료

--- src/context/AuthContext.js
+++ src/context/AuthContext.js
@@ -1,17 +1,27 @@
 import React, { createContext, useContext, useEffect, useState } from "react";
-import { onUserStateChange } from "../api/fbase";
+import { authService, onUserStateChange } from "../api/fbase";
+import { updateCurrentUser } from "firebase/auth";
 
 const AuthContext = createContext();
 
 export function AuthContextProvider({ children }) {
   const [user, setUser] = useState("");
+  //유저 정보 가져오기
   useEffect(() => {
     onUserStateChange((user) => {
       setUser(user);
     });
   }, []);
+
+  //최신 상태 업데이트
+  const refreshUser = async () => {
+    await updateCurrentUser(authService, authService.currentUser);
+    setUser(authService.currentUser);
+  };
   return (
-    <AuthContext.Provider value={{ user }}>{children}</AuthContext.Provider>
+    <AuthContext.Provider value={{ user, refreshUser }}>
+      {children}
+    </AuthContext.Provider>
   );
 }
 
--- src/routes/Profile.js
+++ src/routes/Profile.js
@@ -5,8 +5,10 @@ import { updateProfile } from "firebase/auth";
 import { useAuthContext } from "../context/AuthContext";
 
 export default function Profile() {
-  const { user } = useAuthContext();
+  const { user, refreshUser } = useAuthContext();
   const [newDisplayName, setNewDisplayName] = useState(user.displayName);
+  const [isUpdated, setIsUpdated] = useState(false);
+
   // const getMyNweets = async () => {
   //   const q = query(
   //     collection(dbService, "content"),
@@ -22,6 +24,11 @@ export default function Profile() {
   //   getMyNweets();
   // }, []);
 
+  //수정모드
+  const onUpdateHandler = () => {
+    setIsUpdated((prev) => !prev);
+  };
+  //업데이트 체인지
   const onChange = (e) => {
     const {
       target: { value },
@@ -35,14 +42,25 @@ export default function Profile() {
       await updateProfile(authService.currentUser, {
         displayName: newDisplayName,
       });
+      refreshUser();
     }
   };
 
   return (
     <div>
       <form onSubmit={onSubmit}>
-        <input type="text" value={newDisplayName} onChange={onChange} />
-        <input type="submit" value="프로필 수정" />
+        <input
+          type="text"
+          value={newDisplayName}
+          onChange={onChange}
+          readOnly={!isUpdated}
+        />
+        <button onClick={onUpdateHandler}>{isUpdated ? "취소" : "수정"}</button>
+        {isUpdated && (
+          <>
+            <input type="submit" value="완료" />
+          </>
+        )}
       </form>
     </div>
   );
