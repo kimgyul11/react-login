6113922f680ce358da59d918df3dabc2c39572f3
kimgyul11 <coloo88@naver.com>
2023-08-11 19:40:38 +0900

    HOME-게시글 업로드 영역 loading추가

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -12,9 +12,13 @@ import Content from "../components/Content";
 import { getDownloadURL, ref, uploadString } from "firebase/storage";
 import { useAuthContext } from "../context/AuthContext";
 
-export default function Home({ userObj }) {
+export default function Home() {
+  //state
   const [content, setContent] = useState("");
   const [contents, setContents] = useState([]);
+  const [isUploaded, setIsUploaded] = useState(false);
+
+  //파일 업로드 관련
   const [attachement, setAttachement] = useState(""); //파일 접근
   const fileInput = useRef();
   const { user } = useAuthContext();
@@ -35,11 +39,17 @@ export default function Home({ userObj }) {
     });
   }, []);
 
-  //submit핸들러
+  //게시글 submit핸들러
   const onSubmitHandler = async (e) => {
     e.preventDefault();
+    if (content === "") {
+      alert("내용을 입력해주세요!");
+      return;
+    }
     try {
+      setIsUploaded(true);
       let url = "";
+      //이미지가 있다면 업로드
       if (attachement !== "") {
         const fileRef = ref(storageService, `${user.uid}/${uuidv4()}`);
         await uploadString(fileRef, attachement, "data_url");
@@ -55,8 +65,10 @@ export default function Home({ userObj }) {
       setContent("");
       setAttachement("");
       fileInput.current.value = null;
+      setIsUploaded(false);
     } catch (e) {
       console.error("Error adding document: ", e);
+      setIsUploaded(false);
     }
   };
   const onChange = (e) => {
@@ -104,7 +116,7 @@ export default function Home({ userObj }) {
             onChange={onFileChange}
             ref={fileInput}
           />
-          <input type="submit" value="Nweet" />
+          <input type="submit" value="Nweet" disabled={isUploaded} />
           {attachement && (
             <div>
               <img src={attachement} width="50px" height="50px" />
