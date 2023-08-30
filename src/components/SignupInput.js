35b3f568e74a1a976da5247f299f79b220f61095
kimgyul11 <coloo88@naver.com>
2023-08-30 13:47:42 +0900

    signuppage

--- src/components/SignupInput.js
+++ src/components/SignupInput.js
@@ -6,9 +6,17 @@ export default function SignupInput({ onClickHandler }) {
   const [password, setPassword] = useState("");
 
   //input이벤트
+  const changeEmail = (e) => {
+    setEmail(e.target.value);
+  };
+  const changePassword = (e) => {
+    setPassword(e.target.value);
+  };
 
   //회원가입 submit
-
+  const sumbitHandler = (e) => {
+    e.preventDefault();
+  };
   return (
     <div className={styles.login_wrap}>
       <div className={styles.title_wrap}>
@@ -16,40 +24,43 @@ export default function SignupInput({ onClickHandler }) {
         <br />
         5분이면 OK!
       </div>
-      <div className={styles.contentWrap}>
-        <div className={styles.content}>
-          <div className={styles.inputTitle}>이메일 주소</div>
-          <div className={styles.inputWrap}>
-            <input
-              type="email"
-              placeholder="이메일을 입력하세요"
-              value={email}
-              required
-            />
+      <form>
+        <div className={styles.contentWrap}>
+          <div className={styles.content}>
+            <div className={styles.inputTitle}>이메일 주소</div>
+            <div className={styles.inputWrap}>
+              <input
+                type="email"
+                placeholder="이메일을 입력하세요"
+                onChange={changeEmail}
+                value={email}
+                required
+              />
+            </div>
+          </div>
+          <div className={styles.content}>
+            <div className={styles.inputTitle}>비밀번호</div>
+            <div className={styles.inputWrap}>
+              <input
+                type="password"
+                placeholder="비밀번호"
+                onChange={changePassword}
+                value={password}
+                required
+              />
+            </div>
+          </div>
+          <div className={styles.content}>
+            <div className={styles.inputTitle}>비밀번호 확인</div>
+            <div className={styles.inputWrap}>
+              <input type="email" placeholder="비밀번호" required />
+            </div>
+          </div>
+          <div className={styles.toggle}>
+            <span onClick={onClickHandler}>로그인으로 이동</span>
           </div>
         </div>
-        <div className={styles.content}>
-          <div className={styles.inputTitle}>비밀번호</div>
-          <div className={styles.inputWrap}>
-            <input
-              type="password"
-              placeholder="비밀번호"
-              value={password}
-              required
-            />
-          </div>
-        </div>
-        <div className={styles.content}>
-          <div className={styles.inputTitle}>비밀번호 확인</div>
-          <div className={styles.inputWrap}>
-            <input type="email" placeholder="비밀번호" required />
-          </div>
-        </div>
-        <div className={styles.toggle}>
-          <span onClick={onClickHandler}>로그인으로 이동</span>
-        </div>
-      </div>
-
+      </form>
       <div className={styles.buttonWrap}>
         <button>확인</button>
       </div>
